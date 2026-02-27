1dea023aebbe34750731d65565fe041aff8d87ae
Vineastevia <156806211+Vineastevia@users.noreply.github.com>
2026-02-27 11:43:12 +0100

    add test request api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('front');
+  message = signal('Chargement...');
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/hello', { responseType: 'text' })
+      .subscribe(data => this.message.set (data));
+  }
 }
