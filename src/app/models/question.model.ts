c95752fb889f2a9873280f3b44386fbf8556cc67
Cassandre <cass@2a02-842a-8ac1-3c01-b1ec-a971-2963-b482.rev.sfr.net>
2026-03-06 12:40:30 +0100

    fix : maturity-model

--- src/app/models/question.model.ts
+++ src/app/models/question.model.ts
@@ -1,6 +1,5 @@
 export interface Question {
-  id: number;
-  label: string;
-  description?: string; //optionnel 
-  modelId: number; // une question peut être dans plusieurs model ?
+  id:    number;
+  text:  string;
+  order: number;
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,9 +3,10 @@ import { Role } from './role.enum';
 export interface User {
   id: number;
   email: string; 
+  password: string;
   firstName: string;
   lastName: string;
   role: Role;
   teamId?: number; // un user peut être dans plusieur team ? 
-  createdAt: Date;
+  createdAt?: Date;
 }
